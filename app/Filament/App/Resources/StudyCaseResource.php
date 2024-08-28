04af3b039c1ed734a5ba0066116a842cec427ce7
Dan Tang <dan@stats4sd.org>
2024-08-28 10:19:36 +0100

    commonly use app panel study case form in app panel and admin panel

--- app/Filament/App/Resources/StudyCaseResource.php
+++ app/Filament/App/Resources/StudyCaseResource.php
@@ -344,16 +344,23 @@ class StudyCaseResource extends Resource
                             ->icon('heroicon-m-check-circle')
                             ->disabled($form->getRecord() == null)
                             ->schema([
+
+                                // This form in app panel is commonly used in admin panel.
+                                // We cannot determine whether user is in app panel or in admin panel by checking current class name.
+                                // (because class name will be app panel class name when admin panel called this form in app panel)
+                                // TODO: We need to disable checkbox by checking whether the logged in user is a reviewer
+
                                 // This checkbox is for submitter only, not for reviewer
                                 // It should be disabled in admin panel
+                                // TODO: disable it if logged in user is a reviewer
                                 Forms\Components\Checkbox::make('ready_for_review')
                                     ->label(t('I confirm that all content is correct. This case is now ready for reviewer to review.'))
                                     ->hint(t('This is to be confirmed by case submitter'))
-                                    // ->disabled()
                                     ->columnSpanFull(),
 
                                 // This checkbox is for reviewer only, not for submitter
                                 // It should be disabled in app panel
+                                // TODO: disable it if logged in user is not a reviewer
                                 Forms\Components\Checkbox::make('reviewed')
                                     ->label(t('I confirm that all content has been reviewed. This case is now ready for publishing.'))
                                     ->hint(t('This is to be confirmed by case reviewer'))
