65c85e745b050e6d76fded8760c12fe1fc18b007
Dan Tang <dan@stats4sd.org>
2024-09-16 14:57:37 +0100

    Remove contact person details from team seeder file; Add model has roles seeder file

--- database/seeders/TeamsTableSeeder.php
+++ database/seeders/TeamsTableSeeder.php
@@ -14,37 +14,31 @@ class TeamsTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('teams')->delete();
-        
-        \DB::table('teams')->insert(array (
-            0 => 
-            array (
+
+        \DB::table('teams')->insert(array(
+            0 =>
+            array(
                 'id' => 1,
                 'name' => 'Test User Team',
                 'website' => NULL,
-                'contact_person_name' => NULL,
-                'contact_person_email' => NULL,
                 'description' => NULL,
                 'avatar' => NULL,
                 'created_at' => '2024-08-15 10:22:09',
                 'updated_at' => '2024-08-15 10:22:09',
             ),
-            1 => 
-            array (
+            1 =>
+            array(
                 'id' => 2,
-                'name' => 'Reviewer Team',
+                'name' => 'Stats4SD',
                 'website' => NULL,
-                'contact_person_name' => NULL,
-                'contact_person_email' => NULL,
                 'description' => NULL,
                 'avatar' => NULL,
                 'created_at' => '2024-08-22 09:09:22',
                 'updated_at' => '2024-08-22 09:09:22',
             ),
         ));
-        
-        
     }
 }
