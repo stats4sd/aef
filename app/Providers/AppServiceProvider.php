5bbd6a3bea12fc560a0c1106a165e0bce5af9060
Dan Tang <dan@stats4sd.org>
2024-09-27 09:46:21 +0100

    WIP: revise modal popup behavior

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Filament\Support\View\Components\Modal;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // disable closing the popup modal by clicking away
+        Modal::closedByClickingAway(false);
+
+        // hide the popup modal "Close" button at upper right corner
+        Modal::closeButton(false);
+
+        // TODO: disable closing the popup modal by pressing Escape key
+        // Note: this function does not exist in filament v3.2 Modal class
+        // Modal::closedByEscaping(false);
     }
 }
