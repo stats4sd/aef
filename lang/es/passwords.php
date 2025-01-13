c58c53494c5e46fb6ba4a03d2cc1b8996226d7eb
Ciara <ciara@stats4sd.org>
2025-01-13 15:44:16 +0000

    adds middleware

---
+++ lang/es/passwords.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'reset'     => 'Tu contraseña ha sido restablecida.',
+    'sent'      => 'Te hemos enviado por correo electrónico el enlace para restablecer tu contraseña.',
+    'throttled' => 'Por favor, espera antes de volver a intentarlo.',
+    'token'     => 'Este token de restablecimiento de contraseña es inválido.',
+    'user'      => 'No podemos encontrar un usuario con esa dirección de correo electrónico.',
+];
