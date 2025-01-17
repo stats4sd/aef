2e245018e7cea61fcc67798d1846338789e509ac
Ciara <ciara@stats4sd.org>
2025-01-17 11:28:46 +0000

    changes incorrect label

--- resources/views/cases/products.blade.php
+++ resources/views/cases/products.blade.php
@@ -20,7 +20,7 @@
             $media = $communicationProduct->getMedia('comms_products')->first();
             if($media) {
                 $file_name = $media->file_name;
-                $file_size_mb = Number::fileSize($media->size);
+                $file_size = Number::fileSize($media->size);
                 $file_url = $media->getUrl();
                 $file_type = basename($media->mime_type);
             }
@@ -40,7 +40,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-8 w-8 mr-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>
-                        {{ t("Download ")}} {{ $file_type }} | {{ $file_size_mb}}
+                        {{ t("Download ")}} {{ $file_type }} | {{ $file_size }}
                     </a>
                 @endif
                 <!-- Visit Link Button -->
