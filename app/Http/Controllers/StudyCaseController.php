68f6410974ea0225867a844a06f9f9e2700ed622
Dan Tang <dan@stats4sd.org>
2025-09-11 10:26:41 +0100

    move access control business logic to StudyCasePolicy

--- app/Http/Controllers/StudyCaseController.php
+++ app/Http/Controllers/StudyCaseController.php
@@ -3,60 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\StudyCase;
-use Illuminate\Http\Request;
 use App\Enums\StudyCaseStatus;
 
 class StudyCaseController extends Controller
 {
     public function getData(StudyCase $studycase) {
-        // add access control:
-        // 1. Public can only access study case with status Reviewed
-        // 2. Admin can access study case with all status
-        // 3. All teammates of study case creator can access study case with all status
-
-        // initialse study case cannot be preview by default
-        $canPreview = false;
-
-        // there is no logged in user, assumes it is accessed by public
-        if (auth()->user() == null) {
-            // ray('there is no logged in user');
-            // ray('study case status: ' . $studycase->status->value);
-
-            // only study case with status Reviewed can be accessed by public
-            if ($studycase->status == StudyCaseStatus::Reviewed) {
-                $canPreview = true;
-            }
-        }
-        // there is a logged in user
-        else 
-        {
-            // the logged in user is admin user
-            if (auth()->user()->isAdmin()) {
-                // ray('logged in user is admin user');
-                // admin user can view all study cases with any status
-                $canPreview = true;
-            } 
-            // the logged in user is normal user
-            else 
-            {
-                // ray('logged in user is normal user');
-
-                // ray($studycase->team);
-                // ray(auth()->user()->latestTeam);
-
-                // if the study case is created by a user who belongs to user's team, user can view it with any status
-                if ($studycase->team == auth()->user()->latestTeam) {
-                    $canPreview = true;
-                }
-            }
-        }
-
-        if (!$canPreview) {
+        // StudyCasePolicy.view() will not be called if there is no logged in user
+        if (auth()->user()?->cannot('view', $studycase)) {
             abort(404);
-        } else {
-            return view('cases.index', ['studycase' => $studycase]);
         }
-       
+
+        // add handling if there is no logged in user (public access)
+        // Question: is there any way to call StudyCasePolicy.view() so that all access control business logic can be centralised there?
+        if (auth()->user() == null && $studycase->status != StudyCaseStatus::Reviewed) {
+            abort(404);
+        }
+
+        return view('cases.index', ['studycase' => $studycase]);
     }
 
     public function index()
--- app/Policies/StudyCasePolicy.php
+++ app/Policies/StudyCasePolicy.php
@@ -2,9 +2,9 @@
 
 namespace App\Policies;
 
-use App\Models\StudyCase;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
+use App\Models\StudyCase;
+use App\Enums\StudyCaseStatus;
 
 class StudyCasePolicy
 {
@@ -21,9 +21,51 @@ class StudyCasePolicy
      */
     public function view(User $user, StudyCase $studyCase): bool
     {
-        ray('StudyCasePolicy.view()...');
-        return true;
-        // return false;
+        // ray('StudyCasePolicy.view()...');
+
+        // add access control for viewing study case content in front-end
+        // 1. Public can only access study case with status Reviewed
+        // 2. Admin can access study case with all status
+        // 3. All teammates of study case creator can access study case with all status
+
+        // initialse study case cannot be preview by default
+        $canPreview = false;
+
+        // there is no logged in user, assumes it is accessed by public
+        if ($user == null) {
+            // ray('there is no logged in user');
+            // ray('study case status: ' . $studyCase->status->value);
+
+            // only study case with status Reviewed can be accessed by public
+            if ($studyCase->status == StudyCaseStatus::Reviewed) {
+                $canPreview = true;
+            }
+        }
+        // there is a logged in user
+        else 
+        {
+            // the logged in user is admin user
+            if ($user->isAdmin()) {
+                // ray('logged in user is admin user');
+                // admin user can view all study cases with any status
+                $canPreview = true;
+            } 
+            // the logged in user is normal user
+            else 
+            {
+                // ray('logged in user is normal user');
+
+                // ray($studyCase->team);
+                // ray($user->latestTeam);
+
+                // if the study case is created by a user who belongs to user's team, user can view it with any status
+                if ($studyCase->team == $user->latestTeam) {
+                    $canPreview = true;
+                }
+            }
+        }
+
+        return $canPreview;
     }
 
     /**
