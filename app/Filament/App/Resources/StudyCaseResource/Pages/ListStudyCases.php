7cbd631cc9fbe1bce7ba72fa9c6ab739b48ffce8
Dave Mills <d.e.mills@stats4sd.org>
2025-09-15 17:13:32 +0100

    Add 'view' mode for Basic Information page; Add custom create action;

--- app/Filament/App/Resources/StudyCaseResource/Pages/ListStudyCases.php
+++ app/Filament/App/Resources/StudyCaseResource/Pages/ListStudyCases.php
@@ -3,6 +3,7 @@
 namespace App\Filament\App\Resources\StudyCaseResource\Pages;
 
 use Filament\Actions;
+use Filament\Facades\Filament;
 use Filament\Resources\Pages\ListRecords;
 use App\Filament\App\Resources\StudyCaseResource;
 
@@ -15,7 +16,19 @@ class ListStudyCases extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\Action::make('New Case')
+                ->action(function() {
+
+                    // automatically create a new case study with status proposal and redirect to edit page
+                    $studyCase = \App\Models\StudyCase::create([
+                        'status' => \App\Enums\StudyCaseStatus::Proposal,
+                        'team_id' => Filament::getTenant()?->id ?? auth()->user()->currentTeam->id,
+                    ]);
+
+                    $this->redirect(StudyCaseResource::getUrl('edit-basic-information', ['record' => $studyCase->id]));
+
+
+                }),
         ];
     }
 
