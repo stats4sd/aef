21c61bce87c9fba8edb5f2d1214c2baba5e9f331
Dan Tang <dan@stats4sd.org>
2025-09-15 13:37:50 +0100

    revise Create Your Case page in front-end

--- resources/views/home/create.blade.php
+++ resources/views/home/create.blade.php
@@ -15,18 +15,9 @@
         <h2 class="text-xl lg:text-2xl text-ochre mb-6 lg:mb-8">
             {{ t("CASES AND EVIDENCE") }}
         </h2>
-
-        <div class="flex flex-col lg:flex-row lg:space-x-4 pt-6 lg:pt-12 items-center lg:items-start">
-            <a href="app/register" class="bg-ochre hover-effect text-white font-semibold py-2 px-4 w-fit mb-4 lg:mb-0">
-                {{ t("REGISTER NOW") }}
-            </a>
-        </div>
-
-        <!-- Question: how to make it left align with the button above? -->
-        <div class="flex flex-col lg:flex-row lg:space-x-4 pt-6 lg:pt-12 items-center lg:items-start">
-            <a href="app/login" class="text-ochre hover:underline">{{ t("Login with an existing account") }}</a>
-        </div>
-
+        <h3 class="text-xl lg:text-2xl text-ochre mb-6 lg:mb-8">
+            {{ t("Register an account to make your case for agroecology") }}
+        </h3>
     </div>
 </div>
 
@@ -34,17 +25,21 @@
     <div class="bg-white pt-12 md:py-12 xl:container xl:mx-auto">
 
         <div class="text-lg text-black pb-8 px-12 md:px-20">
-            {!! t("Register an account to make your case for agroecology") !!}
+            {!! t("We welcome new proposals for evidence-based cases. To register on this site, you must be a participant in our online training course 
+            <a href=\"https://courses.stats4sd.org\" class=\"text-ochre hover:underline\" target=\"_blank\">Grassroots Evidence for Agroecology</a>.") !!}
         </div>
 
         <div class="text-lg text-black pb-8 px-12 md:px-20">
-            {!! t("We welcome new proposals for evidence-based cases. To start building your proposal, register an account through the link above. ") !!}
+            {!! t("When you have enrolled in the course, return here and register an account below. You will need your email address and the code provided to you in the course. ") !!}
         </div>
 
-        <div class="text-lg text-black pb-8 px-12 md:px-20">
-            {!! t("If you are a participant in the online training course \"Grassroots Evidence for Agroecology\" during 2025-26, 
-            you have come to the right place! Please register below, using the same email address you use for the course 
-            on our <a href=\"https://courses.stats4sd.org\" class=\"text-ochre hover:underline\">Moodle site</a>") !!}
+        <div class="container mx-auto text-center px-4 sm:px-0">
+            <p><br/></p>
+            <a href="app/register" class="bg-ochre hover-effect text-white font-semibold py-2 px-4 w-fit mb-4 lg:mb-0">
+                {{ t("Register Now") }}
+            </a>
+            <p><br/></p>
+            <a href="app/login" class="text-ochre hover:underline">{{ t("Login with an existing account") }}</a>
         </div>
 
     </div>
