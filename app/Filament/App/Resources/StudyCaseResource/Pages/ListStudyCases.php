0a92548fcff98dbc4d086eb7ab3cbb99b235501e
Dan Tang <dan@stats4sd.org>
2025-09-16 09:45:05 +0100

    fill in default values after creating a new study case

--- app/Filament/App/Resources/StudyCaseResource/Pages/ListStudyCases.php
+++ app/Filament/App/Resources/StudyCaseResource/Pages/ListStudyCases.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\App\Resources\StudyCaseResource\Pages;
 
+use Carbon\Carbon;
 use Filament\Actions;
 use Filament\Facades\Filament;
 use Filament\Resources\Pages\ListRecords;
@@ -20,9 +21,14 @@ class ListStudyCases extends ListRecords
                 ->action(function() {
 
                     // automatically create a new case study with status proposal and redirect to edit page
+                    // fill in default values
+                    // it is also a compromise to an issue that if user clicks Cancel button after creating case, 
+                    // an empty study case will be created
                     $studyCase = \App\Models\StudyCase::create([
                         'status' => \App\Enums\StudyCaseStatus::Proposal,
                         'team_id' => Filament::getTenant()?->id ?? auth()->user()->currentTeam->id,
+                        'title' => 'Untitled',
+                        'year_of_development' => Carbon::now()->format('Y'),
                     ]);
 
                     $this->redirect(StudyCaseResource::getUrl('edit-basic-information', ['record' => $studyCase->id]));
